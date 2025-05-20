ee5752948139f2ccf782b09d2349efb307ec77b6
geracao-11 <geracaotech11@gmail.com>
2025-05-20 10:49:46 -0300

    tirar o reset global de cores

--- .history/src/pages/Hideki/index_20250520104833.jsx
+++ .history/src/pages/Hideki/index_20250520104833.jsx
@@ -3,9 +3,9 @@ import Header from "./components/Header";
 
 const Hideki = () => {
     return (
-        <main className="px-12 py-24 bg-black text-white h-full">
+        <section className="px-12 py-24 bg-black text-whi">
             <Header />
-        </main>
+        </section>
     );
 }
 
--- .history/src/pages/Hideki/index_20250520104903.jsx
+++ .history/src/pages/Hideki/index_20250520104903.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 
 const Hideki = () => {
     return (
-        <main className="px-12 py-24 bg-black text-white h-full">
+        <main className="px-12 py-24 bg-black text-white">
             <Header />
         </main>
     );
